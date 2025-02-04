2048ab05f9afc34e39883a62d5b48641cf2f5679
aitazizhassan <147891406+aitazizhassan@users.noreply.github.com>
2025-02-04 18:34:52 +0100

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -1,122 +1,33 @@
-import { search, download } from 'aptoide-scraper';
-import baileys from '@adiwajshing/baileys';
-const {
-					proto,
-					generateWAMessageFromContent, 
-					prepareWAMessageMedia
-				} = baileys;
+import { download } from 'aptoide-scraper';
 
-async function response(jid, data, quoted) {
-				let msg = generateWAMessageFromContent(jid, {
-					viewOnceMessage: {
-						message: {
-							"messageContextInfo": {
-								"deviceListMetadata": {},
-								"deviceListMetadataVersion": 2
-							},
-							interactiveMessage: proto.Message.InteractiveMessage.create({
-								body: proto.Message.InteractiveMessage.Body.create({
-									text: data.body
-								}),
-								footer: proto.Message.InteractiveMessage.Footer.create({
-									text: data.footer
-								}),
-								header: proto.Message.InteractiveMessage.Header.create({
-									title: data.title,
-									subtitle: data.subtitle,
-									hasMediaAttachment: data. media ? true : false, 
-									...(data.media ? await prepareWAMessageMedia(data.media, { upload: conn.waUploadToServer }) : {})
-								}),
-								nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-									buttons: data.buttons
-								})
-							})
-						}
-					}
-				}, { quoted })
+let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
+  try {
+    if (command === 'modapk') {
+      if (!text) throw `*[❗] الرجاء تقديم اسم تطبيق APK الذي تريد تنزيله.*`;
 
-				await conn.relayMessage(msg.key.remoteJid, msg.message, {
-					messageId: msg.key.id
-				})
-				}
-				
-				/**async function search(string) {
-					let data = await axios.get("https://deliriussapi-oficial.vercel.app/download/apk?query=" + encodeURIComponent(string));
-					return data.data
-					}**/
+      await conn.reply(m.chat, global.wait, m);
+      let data = await download(text);
 
-let handler = async (m, { conn, command, usedPrefix, text }) => {
-    if (command === "apk") {
-        if (!text) throw `نكتب على سبيل التجربة:\n\n${usedPrefix + command} facebook lite`;
-        
-        const data = await search(text);
-        if (!data.length) throw `لم نجد اي تطبيق تحت اسم \n "${text}".`;
+      if (data.size.replace(' MB', '') > 200) {
+        return await conn.sendMessage(m.chat, { text: '*[⛔] الملف كبير جدًا.*' }, { quoted: m });
+      }
 
-        let sections = [
-            {
-                title: 'التطبيقات المتوفرة',
-                highlight_label: 'Top', 
-                rows: data.map(app => ({
-                    title: app.name,
-                    description: `عرض التفاصيل "${app.name}"`,
-                    id: `.apkview ${app.id}`
-                }))
-            }
-        ];
+      if (data.size.includes('GB')) {
+        return await conn.sendMessage(m.chat, { text: '*[⛔] الملف كبير جدًا.*' }, { quoted: m });
+      }
 
-        const listMessage = {
-            text: `Search results for "${text}":`,
-            footer: 'حدد تطبيقًا لعرض تفاصيله',
-            body: 'المرجو اختيار التطبيق الذي تريد تحميله',
-            buttons: [{
-            name: 'single_select', 
-            buttonParamsJson: JSON.stringify({
-            title: 'نتائج بحث التطبيقات', 
-            sections
-            }) 
-            }]
-        };
-
-        await response(m.chat, listMessage, m);
-    } else if (command === "apkview") {
-        if (!text) throw `Usage:\n${usedPrefix + command} <app id>`;
-        
-        const data = (await axios.get("https://deliriussapi-oficial.vercel.app/download/apk?query="+encodeURIComponent(text))).data.data;
-        const details = `*Name:* ${data.name}\n*Last Updated:* ${data.publish}\n*Size:* ${data.size}\n\nتحميل بالضغط على الزر أدناه:`;
-
-        const buttons = [{
-            name: 'quick_reply', 
-            buttonParamsJson: JSON.stringify({
-            	display_text: "تحميل التطبيق الآن", 
-                id: ".apkget "+ data.id
-            })
-        }];
-
-        const buttonMessage = {
-            body: details,
-            footer: 'تحميل التطبيقات من Aptoide',
-            buttons,
-            media: { image: { url: data.image }}
-        };
-
-        await response(m.chat, buttonMessage, m);
-    } else if (command === "apkget") {
-        if (!text) throw `Usage:\n${usedPrefix + command} <package name>`;
-        
-        const data = (await axios.get("https://deliriussapi-oficial.vercel.app/download/apk?query="+encodeURIComponent(text))).data.data;
-
-        await conn.sendMessage(m.chat, { 
-            document: { url: data.download }, 
-            mimetype: 'application/vnd.android.package-archive', 
-            fileName: `${data.name}.apk`, 
-            caption: `تم التحميل بنجاح يا  @${m.sender.split('@')[0]}`,
-            contextInfo: { mentionedJid: [m.sender] }
-        }, { quoted: m });
+      await conn.sendMessage(
+        m.chat,
+        { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
+        { quoted: m }
+      )
     }
+  } catch {
+    throw `*[❗] حدث خطأ. تأكد من تقديم رابط صالح.*`;
+  }
 };
 
-handler.command = ["apk", "apkview", "apkget"];
-handler.help = ["apk"];
-handler.tags = ["downloader"];
-handler.limit = true
+handler.help = ['تطبيق']
+handler.tags = ['downloader']
+handler.command = /^تطبيق|apk$/i;
 export default handler;
